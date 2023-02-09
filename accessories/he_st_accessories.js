9a6152d8495f4adb8808745728608741ac7f47d1
Joel Wetzel <jwetzel1492@gmail.com>
2023-02-09 13:47:38 -0600

    Change how I construct Service.InputSource

--- accessories/he_st_accessories.js
+++ accessories/he_st_accessories.js
@@ -1143,7 +1143,8 @@ function HE_ST_Accessory(platform, group, device, accessory) {
 
         for (var i = 0; i < inputs.length; i++)
         {
-            const inputService = that.accessory.addService(Service.InputSource, device.name + 'input' + i);
+            const uniqueInputId = device.name + 'input' + i;
+            const inputService = new Service.InputSource(uniqueInputId, uniqueInputId);
             inputService.setCharacteristic(Characteristic.ConfiguredName, inputs[i]);
             inputService.setCharacteristic(Characteristic.Identifier, i + 1);
             inputService.setCharacteristic(Characteristic.InputSourceType, Characteristic.InputSourceType.HDMI);
